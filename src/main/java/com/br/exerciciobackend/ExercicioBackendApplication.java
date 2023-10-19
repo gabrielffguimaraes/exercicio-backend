13ba8f6221746adffae3592748b899c5a0b3f65d
Gabriel Ferreira <gabrielffguimaraes@gmail.com>
2023-10-19 00:58:44 -0300

    configurations

---
+++ src/main/java/com/br/exerciciobackend/ExercicioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.br.exerciciobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/exerciciobackend/ExercicioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.exerciciobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
